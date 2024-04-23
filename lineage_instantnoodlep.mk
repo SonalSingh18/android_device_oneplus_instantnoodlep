67c842a95378d2680a83c43fe596171cb37b87f0
Sonal Singh <sonal.singh.19993@gmail.com>
2024-04-23 15:07:22 -0700

    instantnoodlep: Initialize for Evolution X 9.x

--- lineage_instantnoodlep.mk
+++ lineage_instantnoodlep.mk
@@ -11,8 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+EVO_BUILD_TYPE := Community
+BUILD_BCR := false
+PRODUCT_NO_CAMERA := true
 
 PRODUCT_NAME := lineage_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
